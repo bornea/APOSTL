52c41cf81af61b9ecf6200157640fde4cf8fb635
bornea <bornea27@gmail.com>
2016-04-12 16:37:00 -0400

    Added Bioconductor Updater script.

---
+++ Bioconductor_Updater/Bioconductor_Updater.sh
@@ -0,0 +1,3 @@
+R -e "source('https://bioconductor.org/biocLite.R');biocLite('mygene')"
+R -e "source('https://bioconductor.org/biocLite.R');biocLite('clusterProfiler')"
+R -e "source('https://bioconductor.org/biocLite.R');biocLite('org.Hs.eg.db')"
